fbdc2637b71576caa1e6129dfbaf97ecaf22d936
Parth Patel <parth05@gmail.com>
2026-04-17 12:00:27 +0530

    first-commit

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
-const MyForm = () => {
+const librarystore = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    password: ""
+    age:"",
+    Password: ""
+
   });
 
   const [errors, setErrors] = useState({});
@@ -20,10 +22,10 @@ const MyForm = () => {
   // Validate form
   const validate = () => {
     let temp = {};
-    if (!formData.name) temp.name = "Name is required";
-    if (!formData.email) temp.email = "Email is required";
-    if (!formData.password) temp.password = "Password is required";
-
+    if (!formData.name) temp.name = "name is required";
+    if (!formData.email) temp.email = "email is required";
+    if (!formData.age) temp.age = "age is required";
+    if(!formData.Password) temp.Password = "Password is required";
     setErrors(temp);
     return Object.keys(temp).length === 0;
   };
@@ -32,19 +34,19 @@ const MyForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validate()) {
-      alert("Form Submitted Successfully!");
+      alert(" Details Filled Successfully!");
       console.log(formData);
     }
   };
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px", color: "white" }}>
-      <h2>React Form</h2>
+      <h2>Library Store</h2>
       <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }}>
         <div>
           <input
             type="text"
-            name="name"
+            name="Name"
             placeholder="Enter Name"
             value={formData.name}
             onChange={handleChange}
@@ -56,8 +58,8 @@ const MyForm = () => {
         <div>
           <input
             type="email"
-            name="email"
-            placeholder="Enter Email"
+            name="Email"
+            placeholder="Enter email"
             value={formData.email}
             onChange={handleChange}
             style={{ padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }}
@@ -67,8 +69,20 @@ const MyForm = () => {
 
         <div>
           <input
-            type="password"
-            name="password"
+            type="Number"
+            name="Age"
+            placeholder="Enter Age"
+            value={formData.age}
+            onChange={handleChange}
+            style={{ padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }}
+          />
+          <p style={{ color: "red", fontSize: "12px", margin: "5px 0" }}>{errors.age}</p>
+        </div>
+
+        <div>
+          <input
+            type="Text"
+            name="Password"
             placeholder="Enter Password"
             value={formData.password}
             onChange={handleChange}
@@ -85,4 +99,4 @@ const MyForm = () => {
   );
 };
 
-export default MyForm;
+export default librarystore;
